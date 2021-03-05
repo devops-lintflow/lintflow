be40532b8341d480fd583dc0600ee709972115fe
Jia Jia <angersax@sina.com>
2021-03-06 00:21:07 +0800

    Add option of 'all' in test.sh for github ci

--- script/test.sh
+++ script/test.sh
@@ -2,4 +2,13 @@
 
 go env -w GOPROXY=https://goproxy.cn,direct
 
-GIN_MODE=release go test -cover -v -parallel 2 ./...
+if [ "$1" = "all" ]; then
+  GIN_MODE=release go test -cover -v -parallel 2 ./...
+else
+  list="$(go list ./... | grep -v review)"
+  old=$IFS IFS=$'\n'
+  for item in $list; do
+    GIN_MODE=release go test -cover -v -parallel 2 "$item"
+  done
+  IFS=$old
+fi
