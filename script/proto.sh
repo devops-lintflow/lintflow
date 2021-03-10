2823432737a7f47084f900911c2d8a5d792e118f
Jia Jia <angersax@sina.com>
2021-03-10 12:27:47 +0800

    Update script file

--- script/proto.sh
+++ script/proto.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-src=lint
-
-#protoc --go_out=./${src} --go_opt=paths=source_relative --go-grpc_out=./${src} --go-grpc_opt=paths=source_relative ./${src}/lint.proto
 protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative ./lint/lint.proto
