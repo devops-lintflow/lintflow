256648ee5ef9a3082fbc2359a2172db3f5a4a167
Jia Jia <angersax@sina.com>
2021-07-25 20:18:01 +0800

    Update scrpit file for code coverage

--- script/test.sh
+++ script/test.sh
@@ -3,12 +3,12 @@
 go env -w GOPROXY=https://goproxy.cn,direct
 
 if [ "$1" = "all" ]; then
-  GIN_MODE=release go test -cover -v -parallel 2 ./...
+  GIN_MODE=release go test -cover -covermode=atomic -parallel 2 -race -v ./...
 else
   list="$(go list ./... | grep -v review)"
   old=$IFS IFS=$'\n'
   for item in $list; do
-    GIN_MODE=release go test -cover -v -parallel 2 "$item"
+    GIN_MODE=release go test -cover -covermode=atomic -parallel 2 -race -v "$item"
   done
   IFS=$old
 fi
