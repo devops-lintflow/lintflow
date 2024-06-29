b078ea3ec8ea0ff8f849fca6f06e08d142d79daa
Jia Jia <angersax@sina.com>
2024-06-29 19:03:27 +0800

    refactor(lint): update proto files

--- go.mod
+++ go.mod
@@ -4,7 +4,6 @@ go 1.22.0
 
 require (
 	github.com/alecthomas/kingpin/v2 v2.4.0
-	github.com/golang/protobuf v1.5.3
 	github.com/pkg/errors v0.9.1
 	github.com/reviewdog/reviewdog v0.17.1
 	github.com/stretchr/testify v1.8.4
@@ -16,6 +15,7 @@ require (
 require (
 	github.com/alecthomas/units v0.0.0-20211218093645-b94a6e3cc137 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
--- script/proto.sh
+++ script/proto.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
+# Install protoc
+# curl -LO https://github.com/protocolbuffers/protobuf/releases/download/v26.1/protoc-26.1-linux-x86_64.zip
+
+# Install plugins
+# go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+# go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+# export PATH="$PATH:$(go env GOPATH)/bin"
+
+# Install mock
+# go install github.com/golang/mock/mockgen@latest
+# export PATH="$PATH:$(go env GOPATH)/bin"
+
+# Build proto
 protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative ./lint/lint.proto
